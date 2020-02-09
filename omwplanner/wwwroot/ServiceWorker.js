3c79791072f8812443fd61e743cb3d58462568cc
Finnur Bragason <fbragason@outlook.com>
2020-02-09 13:38:33 +0800

    .

--- omwplanner/wwwroot/ServiceWorker.js
+++ omwplanner/wwwroot/ServiceWorker.js
@@ -1,9 +1,88 @@
 const baseURL = '/';
-const indexURL = '/Pages/_Host.cshtml';
+const indexURL = '/index.html';
 const networkFetchEvent = 'fetch';
 const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
+const staticCachePrefix = 'blazor-cache-v';
+const staticCacheName = 'blazor-cache-v7';
+const requiredFiles = [
+    "/_framework/blazor.server.js",
+    "/favicon.ico",
+    "/fontawsome/css/all.css",
+    "/fontawsome/webfonts/fa-brands-400.eot",
+    "/fontawsome/webfonts/fa-brands-400.svg",
+    "/fontawsome/webfonts/fa-brands-400.ttf",
+    "/fontawsome/webfonts/fa-brands-400.woff",
+    "/fontawsome/webfonts/fa-brands-400.woff2",
+    "/fontawsome/webfonts/fa-regular-400.eot",
+    "/fontawsome/webfonts/fa-regular-400.svg",
+    "/fontawsome/webfonts/fa-regular-400.ttf",
+    "/fontawsome/webfonts/fa-regular-400.woff",
+    "/fontawsome/webfonts/fa-regular-400.woff2",
+    "/fontawsome/webfonts/fa-solid-900.eot",
+    "/fontawsome/webfonts/fa-solid-900.svg",
+    "/fontawsome/webfonts/fa-solid-900.ttf",
+    "/fontawsome/webfonts/fa-solid-900.woff",
+    "/fontawsome/webfonts/fa-solid-900.woff2",
+    "/icon-192.png",
+    "/icon-512.png",
+    "/index.html",
+    "/main.js",
+    "/ServiceWorkerRegister.js",
+    "/manifest.json",
+    "/Style.min.css"
+];
+// * listen for the install event and pre-cache anything in filesToCache * //
+self.addEventListener(swInstallEvent, event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(requiredFiles);
+            })
+    );
+});
+self.addEventListener(swActivateEvent, function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (staticCacheName !== cacheName && cacheName.startsWith(staticCachePrefix)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
 self.addEventListener(networkFetchEvent, event => {
-    return fetch(event.request);
+    const requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === baseURL) {
+            event.respondWith(caches.match(indexURL));
+            return;
+        }
+    }
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.ok) {
+                            if (requestUrl.origin === location.origin) {
+                                caches.open(staticCacheName).then(cache => {
+                                    cache.put(event.request.url, response);
+                                });
+                            }
+                        }
+                        return response.clone();
+                    });
+            }).catch(error => {
+                console.error(error);
+            })
+    );
 });
